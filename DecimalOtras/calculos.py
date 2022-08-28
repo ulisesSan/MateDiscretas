b6e28c76432aa0303e9ffb7e5053ca3e236ba9f3
ulises caceres <ulisescaceres@MacBook-Pro-de-ulises.local>
2022-08-28 02:47:52 -0500

    Esta hasta Hexadecimal

--- DecimalOtras/calculos.py
+++ DecimalOtras/calculos.py
@@ -44,15 +44,104 @@ def verNum(numero):
     
     return valueBool
 
+def rango(numero,baseNum):
+    rango = False
+    numero1 = numero[0]
+    numero2 = 0
+    if len(numero) != 1:
+        numero2 = numero[1]
+
+    for i in range(len (numero1)):
+        if int(numero1[i]) >= 0 | int(numero1[i]) <= int(baseNum) - 1:
+            print('entero')
+            rango = True
+        else:
+            rango = False
+            break
+        
+    
+    if len(numero) != 1 :
+        for i in range(len (numero2)):
+            if int(numero2[i]) >= 0 | int(numero2[i]) <= int(baseNum) - 1 :
+                print('decimal')
+                rango = True
+            else:
+                rango = False
+                break
+
+    print('Rango',rango)
+    return rango
+
 def calculos(numero,baseNum):
     print('base en calculos:', baseNum, '\nlos numeros son: ', numero )
 
+def arregloOperaciones(numero,base):
+    arregloEntero = []
+    arregloDecimal = []
+    entero = numero[0]
+    decimal = '0'
+    if len(numero) != 1:
+        decimal = numero[1]
+    for i in range(len (entero)):
+        #Verificamos que el numero ingresado corresponda al que queremos convertir
+        arregloEntero.append(entero[i] + 'x' + str(base) + '^' + str(len(entero)-i-1)+'+')
+        print(arregloEntero[i])
+
+    for i in range(len (decimal)):
+        arregloDecimal.append('+'+decimal[i] + 'x' + str(base) + '^' + str(len(decimal)-(i)-len(decimal)-1))
+
+    res = arregloEntero + arregloDecimal
+    messagebox.showinfo(message= res)
+    print(res)
+
+def operaciones(numero,Base):
+    sum = 0
+    numero1 = numero[0]
+    numero2 = '0'
+    if len(numero) != 1:
+        numero2 = numero[1]
+    arregloEntero = []
+    arregloDecimal = []
+    
+    for i in range(len (numero1)):
+        if int(numero1[i]) >=0 | int(numero1[i]) <= Base :
+            arregloEntero.append(int(numero1[i]) * int(Base)** (len(numero1)-i-1))
+            print(arregloEntero[i])
+            sum = sum +  arregloEntero[i]
+    #Con la longitud del dato decimal iteramos 
+    for j in range(len (str(numero2))):
+        if int(numero2[j]) >=0 | int(numero2[j]) <= int(Base) :
+            arregloDecimal.append(int(numero2[j]) * int(Base)** (len(numero2)-(j)- len(numero2)-1))
+            sum = sum +  arregloDecimal[j]
+            print(len(numero2)-(j)- len(numero2)-1)
+
+    
+
+    messagebox.showinfo(message = 'Resultado: ' + str(sum))
 
 
 def numeros(numero,baseNum):
-    comBase = base(baseNum)
+    comBase = int(base(baseNum))
     print("entra al calculo", numero ," ", baseNum, ' ',comBase)
     valueBool = verNum(numero.split('.'))
     print(valueBool)
-    if valueBool == True:
+    rangoNum = rango(numero.split('.'), comBase)
+
+    if valueBool == True & rangoNum == True:
+        arregloOperaciones(numero.split('.'),comBase)
         calculos(numero.split('.'),baseNum)
+        operaciones(numero.split('.'),comBase)
+
+#arregloEntero = []
+    #arregloDecimal = []
+    #Con la longitud del dato entero iteramos 
+    #for i in range(len (dato)):
+    #    if int(dato[i]) <=0 | int(dato[i]) <= 2 :
+    #        arregloEntero.append(int(dato[i]) * 3** (len(dato)-i-1))
+    #        print(arregloEntero[i])
+    #Con la longitud del dato decimal iteramos 
+    #for j in range(len(decimal)):
+    #    if int(decimal[i]) <=0 | int(decimal[i]) <= 2 :
+    #        arregloDecimal.append(int(decimal[i]) * 3** (len(decimal)-(i)- len(decimal)-1))
+    #        print(len(decimal)-(i)- len(decimal)-1)
+
